cfb713275d8d9fd8975e6182f69b9034d5a95bc3
Galimja <galim12345678@gmail.com>
2023-12-04 03:06:29 +0600

    steps 8-10

---
+++ webapp/component.js
@@ -0,0 +1,27 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function(UIComponent, JSONModel, RecourceModel) {
+    'use strict';
+    
+    return UIComponent.extend("sap.ui.walkthrough.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init : function() {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            
+            // set data model
+            var oData = {
+                recipient: {
+                    name: "UI5"
+                }
+            };
+            var oModel = new JSONModel(oData);
+            this.setModel(oModel);
+
+        }
+    })
+});
